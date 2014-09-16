23642c67da6b266892b99526736060e60664970f
Nishita Singh <nishita.nz@gmail.com>
2014-09-17 09:35:36 +1200

    Open form_validation library globally in constructor. Create new validation rule.

--- application/controllers/registration.php
+++ application/controllers/registration.php
@@ -2,21 +2,27 @@
 
 class Registration extends CI_Controller {
 
+	public function __construct() {
+		parent:: __construct();
+
+		// Library to help with validation of form
+		$this->load->library('form_validation');
+	}
+
 	// Index method runs automatically if no other method is called
 	public function index() {
 
 		// Form helper
 		$this->load->helper('form');
 
-		// Library to help with validation of form
-		$this->load->library('form_validation');
+
 
 		// Rules for validation
 		// Set rule for usename
 		// arg1 = name of form element
 		// arg2 = Part of Error Message
 		// arg3 = list of validation steps separated by pipes. First to last
-		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[40]|is_unique[users.Username]|alpha_dash');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[40]|callback_usernameCheck|is_unique[users.Username]|alpha_dash');
 		$this->form_validation->set_rules('password', 'Password', 'required|min_length[8]|max_length[60]');
 		$this->form_validation->set_rules('password2', 'Confirm Password', 'required|min_length[8]|max_length[60]|matches[password]');
 		$this->form_validation->set_rules('email', 'E-Mail', 'trim|required|valid_email');
@@ -48,6 +54,21 @@ class Registration extends CI_Controller {
 
 		// Footer
 		$this->load->view('templates/footer');
+
+	}
+
+	public function usernameCheck($valueToBeValidated){
+		// Check if the username is 'admin'
+		if($valueToBeValidated == 'admin') {
+			// Validation failed
+			// Preo message and return false
+			$this->form_validation->set_message('usernameCheck', 'Did you seriously think that admin is an unused account?!');
+			return false;
+		}else{
+			// Validation passed
+			return true;
+		}
+
 
 	}
 
